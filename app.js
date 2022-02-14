b7cfc48c16f297fe9ca194fb27944102afd595ce
Ana Caicedo <ana.caicedo@edgeuno.com>
2022-02-13 19:19:25 -0500

    Create a component with a general object reference in parent

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Vue.component('CoinDetail', {
-  props: ['heading', 'changePercent', 'img', 'price'],
+  props: ['coin'],
 
   data() {
     return {
@@ -11,16 +11,20 @@ Vue.component('CoinDetail', {
   methods: {
     toggleShowPrices() {
       this.showPrices = !this.showPrices;
-      this.color = this.color.split('').reverse().join('');
+      // this.coin.color = this.coin.color.split('').reverse().join('');
     }
   },
 
   computed: {
+    title() {
+      return `${this.coin.heading} - ${this.coin.symbol}`
+    },
+
     convertedValue() {
       if (!this.value) {
         return 0
       }
-      return this.value / this.price
+      return this.value / this.coin.price
     }
   },
 
@@ -29,16 +33,16 @@ Vue.component('CoinDetail', {
     <img
       v-on:mouseover="toggleShowPrices"
       v-on:mouseout="toggleShowPrices"
-      v-bind:src="img"
-      v-bind:alt="heading"
+      v-bind:src="coin.img"
+      v-bind:alt="coin.heading"
     />
     <h1 
-      v-bind:class="changePercent > 0 ? 'success' : 'danger'"
+      v-bind:class="coin.changePercent > 0 ? 'success' : 'danger'"
     >
-      {{ heading }}
+      {{ coin.heading }}
       <!-- directivas condicionales - S -->
-      <span v-if="changePercent > 0">👍</span>
-      <span v-else-if="changePercent < 0">👎</span>
+      <span v-if="coin.changePercent > 0">👍</span>
+      <span v-else-if="coin.changePercent < 0">👎</span>
       <span v-else>🤞</span>
       <!-- directivas condicionales - E -->
       <span v-on:click="toggleShowPrices">{{ !showPrices ? '🐵' : '🙈 '}}</span>
@@ -46,6 +50,21 @@ Vue.component('CoinDetail', {
 
     <input type="number" v-model="value" />
     <span> {{ convertedValue }} </span>
+
+    <ul v-show="showPrices">
+    <li
+      class="uppercase"
+      v-bind:class="{
+          success: p.value > coin.price,
+          danger: p.value < coin.price,
+          stable: p.value === coin.price
+      }"
+      v-for="(p, i) in coin.pricesWithDays"
+      v-bind:key="p.day"
+    >
+      {{ p.day }} - {{ p.value }}
+    </li>
+  </ul>
 
   </div>
 
@@ -56,51 +75,31 @@ new Vue({
   el: "#app",
   data() {
     return {
-      heading: "Bitcoin",
-      symbol: 'BTC',
-      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-      changePercent: 0,
-      price: 8400,
-      color: 'f4f4f4',
-      value: 0,
-      pricesWithDays: [
-        { day: 'Lunes', value: 8400 },
-        { day: 'Martes', value: 7900 },
-        { day: 'Miercoles', value: 8200 },
-        { day: 'Jueves', value: 9000 },
-        { day: 'Viernes', value: 9400 },
-        { day: 'Sabado', value: 10000 },
-        { day: 'Domingo', value: 10200 },
-      ],
-      showPrices: false
+      btc: {
+        heading: "Bitcoin",
+        symbol: 'BTC',
+        img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+        changePercent: 0,
+        color: 'f4f4f4',
+        price: 8400,
+
+        pricesWithDays: [
+          { day: 'Lunes', value: 8400 },
+          { day: 'Martes', value: 7900 },
+          { day: 'Miercoles', value: 8200 },
+          { day: 'Jueves', value: 9000 },
+          { day: 'Viernes', value: 9400 },
+          { day: 'Sabado', value: 10000 },
+          { day: 'Domingo', value: 10200 },
+        ],
+      },
     };
   },
 
-  computed: {
-    title() {
-      return `${this.heading} - ${this.symbol}`
-    },
-
-    convertedValue() {
-      if (!this.value) {
-        return 0
-      }
-      return this.value / this.price
-    }
-  },
-
-  watch: {
-    showPrices(newVal, oldVal) {
-      console.log(newVal, oldVal)
-    }
-  },
-
-  methods: {
-    toggleShowPrices() {
-      this.showPrices = !this.showPrices;
-      this.color = this.color.split('').reverse().join('');
-    }
-
-
-  },
+  // methods: {
+  //   toggleShowPrices() {
+  //     this.showPrices = !this.showPrices;
+  //     this.color = this.color.split('').reverse().join('');
+  //   }
+  // },
 });
