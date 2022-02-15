fa40d0cef53bacff7f1ca64b20c7acca69c55e94
Ana Caicedo <ana.caicedo@edgeuno.com>
2022-02-14 22:16:23 -0500

    Fix router adding error page

--- src/router.js
+++ src/router.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home";
+import About from "@/views/About";
+import Error from "@/views/Error";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "error",
+    component: Error,
   },
 ];
 
