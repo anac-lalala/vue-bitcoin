7ee6795b1be44c77f9e8c44c9f1a37f507e9d815
Ana Caicedo <anaccaicedoc@gmail.com>
2022-02-17 20:17:33 -0500

    Fix styling

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,67 +1,69 @@
 <template>
-  <div class="flex flex-col sm:flex-row justify-around items-center">
+  <div class="flex flex-col">
     <div>
       <grid-loader :loading="isLoading" :color="'#68d391'"> </grid-loader>
     </div>
     <template v-if="!isLoading">
-      <div class="flex flex-col items-center">
-        <img
-          class="w-20 h-20 mr-5"
-          :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`"
-          :alt="asset.name"
-        />
-        <h1 class="text-5xl">
-          {{ asset.name }}
-          <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
-        </h1>
-      </div>
-
-      <div class="my-10 flex flex-col">
-        <ul>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Ranking</b>
-            <span># {{ asset.rank }}</span>
-          </li>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
-            <span>{{ $filters.dollarFilter(asset.priceUsd) }}</span>
-          </li>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
-            <span>{{ $filters.dollarFilter(min) }}</span>
-          </li>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
-            <span>{{ $filters.dollarFilter(max) }}</span>
-          </li>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
-            <span>{{ $filters.dollarFilter(avg) }}</span>
-          </li>
-          <li class="flex justify-between">
-            <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
-            <span>{{ $filters.percentFilter(asset.changePercent24Hr) }}</span>
-          </li>
-        </ul>
-      </div>
-
-      <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
-        <button
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Cambiar
-        </button>
-
-        <div class="flex flex-row my-5">
-          <label class="w-full" for="convertValue">
-            <input
-              id="convertValue"
-              type="number"
-              class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            />
-          </label>
+      <div class="flex flex-col lg:flex-row justify-between">
+        <div class="m-5">
+          <img
+            class="w-16 m-auto"
+            :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`"
+            :alt="asset.name"
+          />
+          <h1 class="text-5xl">
+            {{ asset.name }}
+            <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
+          </h1>
         </div>
-        <span class="text-xl"></span>
+
+        <div class="m-5 flex flex-col">
+          <ul>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Ranking</b>
+              <span># {{ asset.rank }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
+              <span>{{ $filters.dollarFilter(asset.priceUsd) }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
+              <span>{{ $filters.dollarFilter(min) }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
+              <span>{{ $filters.dollarFilter(max) }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
+              <span>{{ $filters.dollarFilter(avg) }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
+              <span>{{ $filters.percentFilter(asset.changePercent24Hr) }}</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="m-5 sm:mt-0 flex flex-col justify-center text-center">
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Cambiar
+          </button>
+
+          <div class="flex flex-row my-5">
+            <label class="w-full" for="convertValue">
+              <input
+                id="convertValue"
+                type="number"
+                class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+              />
+            </label>
+          </div>
+          <span class="text-xl"></span>
+        </div>
       </div>
 
       <line-chart
